0661082ba076db78029b68e15394211dde0afbdb
berkbeleli <berkbeleli@gmail.com>
2022-04-06 16:11:26 +0300

    Initial Commit

---
+++ MessageApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MessageApp
+//
+//  Created by Berk Beleli on 2022-04-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MessageApp/MessageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageAppApp.swift
+//  MessageApp
+//
+//  Created by Berk Beleli on 2022-04-06.
+//
+
+import SwiftUI
+
+@main
+struct MessageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
