995900378602e76d6a940813c256c2c979df35f3
berkbeleli <berkbeleli@gmail.com>
2022-04-06 16:55:59 +0300

    Corner radius set for chat menu

--- MessageApp/ContentView.swift
+++ MessageApp/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
             }
             .padding(.top, 10)
             .background(.white)
-            
+            .cornerRadius(30, corners: [.topLeft, .topRight])
             
         }
         .background(Color("Blue"))
