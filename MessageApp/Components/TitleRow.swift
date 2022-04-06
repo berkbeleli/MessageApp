71fd7f4506f78b56380595898e90db7de7fa3ab2
berkbeleli <berkbeleli@gmail.com>
2022-04-06 16:20:31 +0300

    Set up app icons and initiliazers

---
+++ MessageApp/Components/TitleRow.swift
@@ -0,0 +1,22 @@
+//
+//  TitleRow.swift
+//  MessageApp
+//
+//  Created by Berk Beleli on 2022-04-06.
+//
+
+import SwiftUI
+
+struct TitleRow: View {
+    var imageUrl = URL(string: "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg")
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TitleRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleRow()
+    }
+}
